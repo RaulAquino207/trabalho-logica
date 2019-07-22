714efa96f692ece41721907d10bfa14e7a86b959
Raul Aquino <aquinoraul207@gmail.com>
2019-07-22 20:38:39 -0300

    part 1/2 almost finished

--- funcoes_projeto.py
+++ funcoes_projeto.py
@@ -31,10 +31,12 @@ def analisar_satisfatibilidade():
                 print(linha)
     cursos_cadastrados = cursos_cadastrados.split(' ')
     cursos_cadastrados.pop()
-    print(cursos_cadastrados)
+    # print(cursos_cadastrados)
 
-    for valores in cursos_cadastrados:
-        print(valores)
+    # for valores in cursos_cadastrados:
+    #     print(valores)
+
+    verificando_cursos(cursos_cadastrados)
 
     manipulador_arquivo.close()
 
@@ -47,20 +49,44 @@ def verificando_cursos(cursos_cadastrados):
     querem_tres_e_quatro = False
     querem_todos = False
 
-    if cursos_cadastrados == '1,2' or cursos_cadastrados == '2,1':
-        querem_um_e_dois = True
-    elif cursos_cadastrados == '1,3' or cursos_cadastrados == '3,1':
-        querem_um_e_tres = True
-    elif cursos_cadastrados == '1,4' or cursos_cadastrados == '4,1':
-        querem_um_e_quatro = True
-    elif cursos_cadastrados == '2,3' or cursos_cadastrados == '3,2':
-        querem_dois_e_tres = True
-    elif cursos_cadastrados == '2,4' or cursos_cadastrados == '4,2':
-        querem_dois_e_quatro = True
-    elif cursos_cadastrados == '3,4' or cursos_cadastrados == '4,3':
-        querem_tres_e_quatro = True
-    elif cursos_cadastrados == '1,2,3,4':
-        querem_todos = True
+    disponibilidade_horarios = []
+
+    for index, valores in enumerate(cursos_cadastrados):
+
+        if '1,2' in cursos_cadastrados[index] or '2,1' in cursos_cadastrados[index]:
+            querem_um_e_dois = True
+        if '1,3' in cursos_cadastrados[index] or '3,1' in cursos_cadastrados[index]:
+            querem_um_e_tres = True
+        if '1,4' in cursos_cadastrados[index] or '4,1' in cursos_cadastrados[index]:
+            querem_um_e_quatro = True
+        if '2,3' in cursos_cadastrados[index] or '3,2' in cursos_cadastrados[index]:
+            querem_dois_e_tres = True
+        if '2,4' in cursos_cadastrados[index] or '4,2' in cursos_cadastrados[index]:
+            querem_dois_e_quatro = True
+        if '3,4' in cursos_cadastrados[index] or '4,3' in cursos_cadastrados[index]:
+            querem_tres_e_quatro = True
+        if '1,2,3,4' in cursos_cadastrados[index]:
+            querem_todos = True
+
+    disponibilidade_horarios.append(querem_um_e_dois)
+    disponibilidade_horarios.append(querem_um_e_tres)
+    disponibilidade_horarios.append(querem_um_e_quatro)
+    disponibilidade_horarios.append(querem_dois_e_tres)
+    disponibilidade_horarios.append(querem_dois_e_quatro)
+    disponibilidade_horarios.append(querem_tres_e_quatro)
+    count_aux = 0
+
+    for index , cursos in enumerate(disponibilidade_horarios):
+        if disponibilidade_horarios[index] == True:
+            count_aux += 1
+    if count_aux == 6:
+        print('Não é possivel oferecer 4 cursos em 3 horas')
+    elif querem_todos == True:
+        print('Não é possivel oferecer 4 cursos em 3 horas')
+    else:
+        print('Ainda é possivel oferecer os cursos 3 horas')
+
+    # print(disponibilidade_horarios)
 
 
 
--- projeto_logica.py
+++ projeto_logica.py
@@ -1,14 +1,17 @@
 from funcoes_projeto import imprimir_cursos, cadastrar_cursos, analisar_satisfatibilidade
-analisar_satisfatibilidade()
+# analisar_satisfatibilidade()
+
 validacao = True
 while(validacao):
     imprimir_cursos()
     print('')
-    print('Para cadastrar cursos digite 1\nPara sair digite 2')
+    print('Para cadastrar cursos digite 1\nPara verificar se é possível ofertar os cursos preditos digite 2\n'
+          'Para sair digite 3')
     op = int(input('Digite a opção desejada: '))
     if op == 1:
         cursos_desejados = []
         qtd_cursos = int(input('Informe quantos cursos você deseja realizar: '))
+
         if qtd_cursos == 4:
             todos_os_cursos = '1,2,3,4'
             cadastrar_cursos(todos_os_cursos)
@@ -19,10 +22,16 @@ while(validacao):
                 cursos_desejados.append(curso_desejado)
                 envio_arquivo = ','.join(cursos_desejados)
             cadastrar_cursos(envio_arquivo)
+
         else:
             print('Quatidade de cursos inválida')
+
     elif op == 2:
+        analisar_satisfatibilidade()
+
+    elif op == 3:
         print('Obrigado!')
         validacao = False
+
     else:
         print('Valor inválido digite novamente')
