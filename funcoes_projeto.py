ff2cfa6e8d03ed96d9924c3094021b6acf6c9857
Raul Aquino <aquinoraul207@gmail.com>
2019-07-22 14:40:57 -0300

    updated in file funcoes_projeto

--- funcoes_projeto.py
+++ funcoes_projeto.py
@@ -34,11 +34,33 @@ def analisar_satisfatibilidade():
     print(cursos_cadastrados)
 
     for valores in cursos_cadastrados:
-
-        if valores == '1,2' or valores == '1,2':
-            print('OK')
-        elif valores == '1,2' or valores == '1,2':
-            print('OK')
-
+        print(valores)
 
     manipulador_arquivo.close()
+
+def verificando_cursos(cursos_cadastrados):
+    querem_um_e_dois = False
+    querem_um_e_tres = False
+    querem_um_e_quatro = False
+    querem_dois_e_tres = False
+    querem_dois_e_quatro = False
+    querem_tres_e_quatro = False
+    querem_todos = False
+
+    if cursos_cadastrados == '1,2' or cursos_cadastrados == '2,1':
+        querem_um_e_dois = True
+    elif cursos_cadastrados == '1,3' or cursos_cadastrados == '3,1':
+        querem_um_e_tres = True
+    elif cursos_cadastrados == '1,4' or cursos_cadastrados == '4,1':
+        querem_um_e_quatro = True
+    elif cursos_cadastrados == '2,3' or cursos_cadastrados == '3,2':
+        querem_dois_e_tres = True
+    elif cursos_cadastrados == '2,4' or cursos_cadastrados == '4,2':
+        querem_dois_e_quatro = True
+    elif cursos_cadastrados == '3,4' or cursos_cadastrados == '4,3':
+        querem_tres_e_quatro = True
+    elif cursos_cadastrados == '1,2,3,4':
+        querem_todos = True
+
+
+
--- projeto_logica.py
+++ projeto_logica.py
@@ -1,28 +1,28 @@
 from funcoes_projeto import imprimir_cursos, cadastrar_cursos, analisar_satisfatibilidade
 analisar_satisfatibilidade()
-# validacao = True
-# while(validacao):
-#     imprimir_cursos()
-#     print('')
-#     print('Para cadastrar cursos digite 1\nPara sair digite 2')
-#     op = int(input('Digite a opção desejada: '))
-#     if op == 1:
-#         cursos_desejados = []
-#         qtd_cursos = int(input('Informe quantos cursos você deseja realizar: '))
-#         if qtd_cursos == 4:
-#             todos_os_cursos = '1,2,3,4'
-#             cadastrar_cursos(todos_os_cursos)
-#
-#         elif qtd_cursos < 4 and qtd_cursos > 0:
-#             for i in range(qtd_cursos):
-#                 curso_desejado = input('Informe o(s) curso(s) desejado(s): ')
-#                 cursos_desejados.append(curso_desejado)
-#                 envio_arquivo = ','.join(cursos_desejados)
-#             cadastrar_cursos(envio_arquivo)
-#         else:
-#             print('Quatidade de cursos inválida')
-#     elif op == 2:
-#         print('Obrigado!')
-#         validacao = False
-#     else:
-#         print('Valor inválido digite novamente')
+validacao = True
+while(validacao):
+    imprimir_cursos()
+    print('')
+    print('Para cadastrar cursos digite 1\nPara sair digite 2')
+    op = int(input('Digite a opção desejada: '))
+    if op == 1:
+        cursos_desejados = []
+        qtd_cursos = int(input('Informe quantos cursos você deseja realizar: '))
+        if qtd_cursos == 4:
+            todos_os_cursos = '1,2,3,4'
+            cadastrar_cursos(todos_os_cursos)
+
+        elif qtd_cursos < 4 and qtd_cursos > 0:
+            for i in range(qtd_cursos):
+                curso_desejado = input('Informe o(s) curso(s) desejado(s): ')
+                cursos_desejados.append(curso_desejado)
+                envio_arquivo = ','.join(cursos_desejados)
+            cadastrar_cursos(envio_arquivo)
+        else:
+            print('Quatidade de cursos inválida')
+    elif op == 2:
+        print('Obrigado!')
+        validacao = False
+    else:
+        print('Valor inválido digite novamente')
